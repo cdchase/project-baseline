830429203cad1a4a8885f81a822bd9976eaeee36
C. Daniel Chase <dan@cdchase.com>
2016-05-30 14:34:32 -0400

    Vagratn & Puppet config for two servers. web: nginx with php-fpm; db: (database not configured.)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # project-baseline
 A baseline setup for any development project using Vagrant and Puppet for setup.
+
+Puppet configuration is managed by Puppet Librarian. Install with:
+
+`gem install librarian-puppet`
+
+Currently using librarian-puppet version 2.2.3, with Ruby version 2.3.1.
--- manifests/default.pp
+++ manifests/default.pp
@@ -13,21 +13,72 @@ class { 'firewall': }
 
 class { 'nginx': }
 
-web::nginx_ssl_with_redirect { 'localhost':
+/* Web configuration */
+
+$full_web_path = '/var/www'
+
+define web::nginx_ssl_with_redirect (
+  # $backend_port         = 9000,
+  $php                  = true,
+  $ssl                  = false,
+  $ssl_only             = false,
+  $proxy                = undef,
+  $www_root             = "${full_web_path}/${name}/",
+  $location_cfg_append  = undef,
+) {
+
+  nginx::resource::vhost { "${name}":
+    ensure              => present,
+    www_root            => $www_root,
+    #    location_cfg_append => { 'rewrite' => '^ https://$server_name$request_uri? permanent' },
+  }
+  if !$www_root {
+    $tmp_www_root = undef
+  } else {
+    $tmp_www_root = $www_root
+  }
+
+  if $php {
+    php::ini { '/etc/php.ini':
+      date_timezone => "America/New_York",
+    }
+    nginx::resource::location { "${name}_root":
+#      ensure              => present,
+      ssl                 => $ssl,
+      ssl_only            => $ssl_only,
+      vhost               => "${name}",
+      www_root            => $www_root,
+      location            => '~ \.php$',
+      index_files         => ['index.php', 'index.html', 'index.htm'],
+      proxy               => undef,
+      #      fastcgi         => "127.0.0.1:${backend_port}",
+      fastcgi             => 'unix:/var/run/php-fpm.sock',
+      fastcgi_script      => undef,
+      location_cfg_append => {
+        fastcgi_connect_timeout => '3m',
+        fastcgi_read_timeout    => '3m',
+        fastcgi_send_timeout    => '3m'
+      }
+    }
+  }
+}
+
+web::nginx_ssl_with_redirect { 'baseline':
   www_root => "/var/www/html",
 }
 
 include php::fpm::daemon
+
 php::fpm::conf { 'www':
-#  listen  => '127.0.0.1:9001',
-  listen => '/var/run/php-fpm.sock',
+  #  listen  => '127.0.0.1:9001',
+  listen  => '/var/run/php-fpm.sock',
   user    => 'nginx',
-# For the user to exist
+  # For the user to exist
   require => Package['nginx'],
 }
 
 firewall { '100 allow http and https access':
-  port   => [80, 443],
+  dport   => [80, 443],
   proto  => tcp,
   action => accept,
 }
